3bed0ac9e314ab6856fe000aee30c1501c806cb2
unknown <rahulrao0209@gmail.com>
2022-04-27 10:40:09 +0530

    debugging

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 const puppeteer = require('puppeteer');
 
+// Selectors 
+const signup = '#__next > main > header > div > div > div.header__actions > div.header__actions-item.header__actions-item--sign-in > a';
+const signInWithPhone = '#main > div > div.page__layout > div.page__content > main > div > div.registration__form > form > div:nth-child(3) > div > span > span > span';
+const inputCode = '#main > div > div.page__layout > div.page__content > main > div > div.registration__form > form > div.form__control.form__control--vertical > div.form__field > div';
+const continueBtn = '#main > div > div.page__layout > div.page__content > main > div > div.registration__form > form > div:nth-child(4) > button';
+const passBtn = '#main > div > div.page__layout > main > div.page__content-inner > div > div > span > div.encounters-user__controls > div > div:nth-child(2) > div > div:nth-child(2) > div > div.encounters-action__icon';
+const smashBtn = '#main > div > div.page__layout > main > div.page__content-inner > div > div > span > div.encounters-user__controls > div > div:nth-child(2) > div > div:nth-child(4) > div > div.encounters-action__icon';
+
 // Generate a random delay value
 const randomValueGenerator = (max) => {
-  return Math.round(Math.random() * max);
+  return Math.round(Math.random() * max) * 1000;
 }
+
+// Generic Swipe Logic
+const swipe = async (page) => {    
+    const delay = randomValueGenerator(7);
+
+    console.log("Page: ", page);
+    
+    await page.waitForNavigation();
+    const pass = await page.$(passBtn);
+    console.log("PASS: ", pass);
+    // if(pass) {
+    //   console.log("PASS BUTTON: ",  page.$(passBtn));
+    //   // await page.click(passBtn);
+    // }
+
+  //   for(let count = 0; count < 10; count++) {
+  //     console.log("Count: " + count);
+
+  //     try {
+        
+  //         if(count % 4 === 0 || count % 7 === 0 || count % 9 === 0) {
+  //           await page.waitForTimeout(delay);
+  //           await page.click(passBtn);
+  //         } else {
+  //           await page.waitForTimeout(delay);
+  //           await page.click(smashBtn);
+  //         }
+  //     } catch(e) {
+  //       console.log("Smash Pass Error: ", e);
+  //     }
+  // }
+}
+
 
 (async () => {
 
@@ -20,31 +61,44 @@ const randomValueGenerator = (max) => {
   await page.goto('https://bumble.com/en/the-buzz/bumble-web-the-same-experience-without-your-phone');
 
   // Click on signup
-  await page.click('#__next > main > header > div > div > div.header__actions > div.header__actions-item.header__actions-item--sign-in > a');
+  if(await page.$(signup)) {
+    await page.click(signup);
+  }
 
   // New page opens, click on signin in with phone number
   try {
     const pages = await browser.pages();
     const latestPage = pages[1];
     await latestPage.waitForNavigation();
-    await latestPage.click('#main > div > div.page__layout > div.page__content > main > div > div.registration__form > form > div:nth-child(3) > div > span > span > span');
+
+    if(await latestPage.$(signInWithPhone)) {
+      await latestPage.click(signInWithPhone);
+    }
 
     // Enter the phone number
     await latestPage.keyboard.type('8369777276');
 
     // Click to continue button to login
-    await latestPage.click('#main > div > div.page__layout > div.page__content > main > div > div.registration__form > form > div:nth-child(4) > button');
+    await latestPage.click(continueBtn);
 
     // Prompt the user for the code
     const prompt = require('prompt-sync')();
     const userCode = prompt('Enter the code: ');
 
     // Focus on the input element and enter the userCode
-    await latestPage.focus('#main > div > div.page__layout > div.page__content > main > div > div.registration__form > form > div.form__control.form__control--vertical > div.form__field > div');
+    await latestPage.focus(inputCode);
     await latestPage.keyboard.type(userCode);
+    
+    const freshPage = await browser.newPage();
+    const appPage = await freshPage.goto('https://bumble.com/app');
+  
 
-    // Get a random delay value
-    const delay = randomValueGenerator(7);
+    // Swipe
+    try {
+      swipe(appPage);
+    } catch (e) {
+      console.log("Exception", e);
+    }
     
   } catch (e) {
     console.log("Signup error", e);
